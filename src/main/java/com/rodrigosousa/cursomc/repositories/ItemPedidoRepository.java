bd6fe01e962a8e586bb2b205241b5a7ed47a86ad
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-19 17:52:38 -0400

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/rodrigosousa/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigosousa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigosousa.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
