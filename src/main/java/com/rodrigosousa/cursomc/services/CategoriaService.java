13f6ceae95a30d27bd20388668ccdb57c838de6e
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-23 15:06:38 -0400

    DELETE Categoria

--- src/main/java/com/rodrigosousa/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigosousa/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rodrigosousa.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rodrigosousa.cursomc.domain.Categoria;
 import com.rodrigosousa.cursomc.repositories.CategoriaRepository;
+import com.rodrigosousa.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigosousa.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 
 }
