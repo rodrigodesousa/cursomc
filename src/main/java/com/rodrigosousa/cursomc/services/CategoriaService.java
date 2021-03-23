a326d206279c598ec6e586f6e0ee429fab685b32
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-23 16:33:35 -0400

    Validacao sintatica com Bean Validation

--- src/main/java/com/rodrigosousa/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigosousa/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigosousa.cursomc.domain.Categoria;
+import com.rodrigosousa.cursomc.dto.CategoriaDTO;
 import com.rodrigosousa.cursomc.repositories.CategoriaRepository;
 import com.rodrigosousa.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigosousa.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
