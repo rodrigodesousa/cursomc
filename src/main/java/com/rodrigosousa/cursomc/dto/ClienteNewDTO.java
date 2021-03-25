96d3bc13469fd36b2bce38dd86de29cb65d9acc4
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-25 16:57:09 -0400

    Adicionando senha a Cliente

--- src/main/java/com/rodrigosousa/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/rodrigosousa/cursomc/dto/ClienteNewDTO.java
@@ -23,7 +23,11 @@ public class ClienteNewDTO implements Serializable {
 	
 	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	private String senha;
 
 	@NotEmpty(message="Preenchimento Obrigatório")
 	private String logradouro;
@@ -151,5 +155,13 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
