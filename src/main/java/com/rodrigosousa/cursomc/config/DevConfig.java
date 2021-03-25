803b3d0dd44126099fceac8459fa5ad3612e1623
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-25 14:47:36 -0400

    Implementando SmtpService com servidor do Google

--- src/main/java/com/rodrigosousa/cursomc/config/DevConfig.java
+++ src/main/java/com/rodrigosousa/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rodrigosousa.cursomc.services.DBService;
+import com.rodrigosousa.cursomc.services.EmailService;
+import com.rodrigosousa.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbservice.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
