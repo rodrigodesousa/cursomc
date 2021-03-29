562dd8b0913f7eba09430f17b8f04e0a1f00a2f4
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-03-29 16:12:44 -0400

    Usando padrao de nomes para imagens

--- src/main/java/com/rodrigosousa/cursomc/services/ClienteService.java
+++ src/main/java/com/rodrigosousa/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.rodrigosousa.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		
@@ -117,13 +125,17 @@ public class ClienteService {
 		if(user == null) {
 			throw new AuthorizationException("Acesso negado!");
 		}
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		URI uri = s3service.uploadFile(multipartFile);
-		Cliente cli = find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
+		return s3service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 		
-		return uri;
+		// Inserir uri de imagem em Cliente
+		// URI uri = s3service.uploadFile(multipartFile);
+		// Cliente cli = find(user.getId());
+		// cli.setImageUrl(uri.toString());
+		// repo.save(cli);
+		// return uri;
 	}
 
 }
